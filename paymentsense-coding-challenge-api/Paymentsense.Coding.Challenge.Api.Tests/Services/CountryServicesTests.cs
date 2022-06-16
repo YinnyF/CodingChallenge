a09ae0e8cd71921d9350486defd1c9ee4eeb0136
AzureAD\CynthiaFu <cynthia.fu@paymentsense.com>
2022-06-16 10:50:11 +0100

    test: add CountryService test for return type

--- paymentsense-coding-challenge-api/Paymentsense.Coding.Challenge.Api.Tests/Services/CountryServicesTests.cs
+++ paymentsense-coding-challenge-api/Paymentsense.Coding.Challenge.Api.Tests/Services/CountryServicesTests.cs
@@ -1,6 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 using Moq;
 using Paymentsense.Coding.Challenge.Api.HttpClientServices;
+using Paymentsense.Coding.Challenge.Api.Models;
 using Paymentsense.Coding.Challenge.Api.Services;
 using Xunit;
 
@@ -28,6 +32,30 @@ namespace Paymentsense.Coding.Challenge.Api.Tests.Services
         }
 
         [Fact]
+        public async Task GetCountriesAsync_ClientReturnsListOfCountries_ReturnsListOfCountries()
+        {
+            // Arrange
+            _countryClient = new Mock<ICountryClient>();
+
+            IList<Country> fakeCountries = new List<Country>()
+            {
+                new Country() { Name = "Test1" },
+                new Country() { Name = "Test2" },
+                new Country() { Name = "Test3" }
+            };
+
+            _countryClient.Setup(s => s.GetCountriesAsync()).ReturnsAsync(fakeCountries);
+
+            _countryService = new CountryService(_countryClient.Object);
+
+            // Act
+            var result = await _countryService.GetCountriesAsync();
+
+            // Assert
+            result.Should().BeOfType<List<Country>>().Which.Should().BeEquivalentTo(fakeCountries);
+        }
+
+        [Fact]
         public async Task GetCountryByAlpha2CodeAsync_CallsGetCountryByAlpha2CodeAsync()
         {
             // Arrange
@@ -44,6 +72,24 @@ namespace Paymentsense.Coding.Challenge.Api.Tests.Services
             _countryClient.Verify(c => c.GetCountryByAlpha2CodeAsync(alpha2Code));
         }
 
-        // TODO: CheckoutException returns something
+        [Fact]
+        public async Task GetCountryByAlpha2CodeAsync_ClientReturnsCountry_ReturnsCountry()
+        {
+            // Arrange
+            _countryClient = new Mock<ICountryClient>();
+            // fyi not concerned if code is valid or not here
+            string validAlpha2Code = "GB";
+            Country fakeCountry = new Country() { Name = "Test1" };
+
+            _countryClient.Setup(s => s.GetCountryByAlpha2CodeAsync(validAlpha2Code)).ReturnsAsync(fakeCountry);
+
+            _countryService = new CountryService(_countryClient.Object);
+
+            // Act
+            var result = await _countryService.GetCountryByAlpha2CodeAsync(validAlpha2Code);
+
+            // Assert
+            result.Should().BeOfType<Country>().Which.Should().BeEquivalentTo(fakeCountry);
+        }
     }
 }
