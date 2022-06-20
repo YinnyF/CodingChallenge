b3186339ac476af55b45e2db49bc38f48d2d2ee0
AzureAD\CynthiaFu <cynthia.fu@paymentsense.com>
2022-06-20 11:40:57 +0100

    feat: add pagination (w/ bugs) conflicts with caching

--- paymentsense-coding-challenge-api/Paymentsense.Coding.Challenge.Api/Controllers/PaymentsenseCodingChallengeController.cs
+++ paymentsense-coding-challenge-api/Paymentsense.Coding.Challenge.Api/Controllers/PaymentsenseCodingChallengeController.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Paymentsense.Coding.Challenge.Api.Filter;
 using Paymentsense.Coding.Challenge.Api.Models;
 using Paymentsense.Coding.Challenge.Api.Services;
+using Paymentsense.Coding.Challenge.Api.Wrappers;
 
 namespace Paymentsense.Coding.Challenge.Api.Controllers
 {
@@ -26,13 +28,23 @@ namespace Paymentsense.Coding.Challenge.Api.Controllers
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         // Not required
         // [ResponseCache(Duration = 60, Location = ResponseCacheLocation.Any, NoStore = false)]
-        public async Task<IActionResult> Get()
+        public async Task<IActionResult> Get([FromQuery] PaginationFilter filter)
         {
+            // this validates the filter object - the query string could contain invalid values.
+            var validFilter = new PaginationFilter(filter.PageNumber, filter.PageSize);
+
             var countries = await _countryService.GetCountriesAsync();
+
+            var totalCountries = countries.Count();
+
+            var pagedCountries = countries
+                .Skip((validFilter.PageNumber - 1) * validFilter.PageSize)
+                .Take(validFilter.PageSize)
+                .ToList();
 
             if (countries != null)
             {
-                return Ok(countries);
+                return Ok(new PagedResponse<IList<Country>>(pagedCountries, validFilter.PageNumber, validFilter.PageSize));
             }
 
             return NotFound();
---
+++ paymentsense-coding-challenge-api/Paymentsense.Coding.Challenge.Api/Wrappers/PagedResponse.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Paymentsense.Coding.Challenge.Api.Wrappers
+{
+    public class PagedResponse<T>
+    {
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public Uri FirstPage { get; set; }
+        public Uri LastPage { get; set; }
+        public int TotalPages { get; set; }
+        public int TotalRecords { get; set; }
+        public Uri NextPage { get; set; }
+        public Uri PreviousPage { get; set; }
+
+        public T Data { get; set; }
+        public string Message { get; set; }
+        public bool Succeeded { get; set; }
+        public string[] Errors { get; set; }
+
+        public PagedResponse(T data, int pageNumber, int pageSize)
+        {
+            this.PageNumber = pageNumber;
+            this.PageSize = pageSize;
+            this.Data = data;
+            this.Message = null;
+            this.Succeeded = true;
+            this.Errors = null;
+        }
+
+    }
+}
