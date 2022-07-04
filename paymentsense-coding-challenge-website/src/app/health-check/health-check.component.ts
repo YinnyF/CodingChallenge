c3e0c4fc443b7c4c34902fb7c8b0f89a85c0f466
AzureAD\CynthiaFu <cynthia.fu@paymentsense.com>
2022-07-04 14:18:59 +0100

    refactor: health check component, unsubscribe

--- paymentsense-coding-challenge-website/src/app/health-check/health-check.component.ts
+++ paymentsense-coding-challenge-website/src/app/health-check/health-check.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { PaymentsenseCodingChallengeApiService } from '../services';
 import { take } from 'rxjs/operators';
 
 import { faThumbsUp, faThumbsDown } from '@fortawesome/free-regular-svg-icons';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-health-check',
@@ -10,34 +11,40 @@ import { faThumbsUp, faThumbsDown } from '@fortawesome/free-regular-svg-icons';
   styleUrls: ['./health-check.component.scss']
 })
 
-export class HealthCheckComponent implements OnInit {
+export class HealthCheckComponent implements OnInit, OnDestroy {
   public faThumbsUp = faThumbsUp;
   public faThumbsDown = faThumbsDown;
   public title = 'Paymentsense Coding Challenge!';
   public paymentsenseCodingChallengeApiIsActive = false;
   public paymentsenseCodingChallengeApiActiveIcon = this.faThumbsDown;
   public paymentsenseCodingChallengeApiActiveIconColour = 'red';
+  private healthSubscription: Subscription;
 
-  constructor(private paymentsenseCodingChallengeApiService: PaymentsenseCodingChallengeApiService) {
-    paymentsenseCodingChallengeApiService.getHealth().pipe(take(1))
-    .subscribe(
-      apiHealth => {
-        this.paymentsenseCodingChallengeApiIsActive = apiHealth === 'Healthy';
-        this.paymentsenseCodingChallengeApiActiveIcon = this.paymentsenseCodingChallengeApiIsActive
-          ? this.faThumbsUp
-          : this.faThumbsUp;
-        this.paymentsenseCodingChallengeApiActiveIconColour = this.paymentsenseCodingChallengeApiIsActive
-          ? 'green'
-          : 'red';
-      },
-      _ => {
-        this.paymentsenseCodingChallengeApiIsActive = false;
-        this.paymentsenseCodingChallengeApiActiveIcon = this.faThumbsDown;
-        this.paymentsenseCodingChallengeApiActiveIconColour = 'red';
-      });
-   }
+  constructor(
+    private paymentsenseCodingChallengeApiService: PaymentsenseCodingChallengeApiService
+  ) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.healthSubscription = this.paymentsenseCodingChallengeApiService.getHealth().pipe(take(1))
+      .subscribe(
+        apiHealth => {
+          this.paymentsenseCodingChallengeApiIsActive = apiHealth === 'Healthy';
+          this.paymentsenseCodingChallengeApiActiveIcon = this.paymentsenseCodingChallengeApiIsActive
+            ? this.faThumbsUp
+            : this.faThumbsUp;
+          this.paymentsenseCodingChallengeApiActiveIconColour = this.paymentsenseCodingChallengeApiIsActive
+            ? 'green'
+            : 'red';
+        },
+        _ => {
+          this.paymentsenseCodingChallengeApiIsActive = false;
+          this.paymentsenseCodingChallengeApiActiveIcon = this.faThumbsDown;
+          this.paymentsenseCodingChallengeApiActiveIconColour = 'red';
+        });
+  }
+
+  ngOnDestroy(): void {
+    this.healthSubscription.unsubscribe();
   }
 
 }
