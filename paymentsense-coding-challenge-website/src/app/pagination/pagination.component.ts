146646e7bd44e8e4dcd150be5c689217e2e8d42e
AzureAD\CynthiaFu <cynthia.fu@paymentsense.com>
2022-07-05 11:26:21 +0100

    refactor: emit total page value when totalPages given as input

--- paymentsense-coding-challenge-website/src/app/pagination/pagination.component.ts
+++ paymentsense-coding-challenge-website/src/app/pagination/pagination.component.ts
@@ -1,14 +1,21 @@
-import { Component, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { Observable, Subject, Subscription } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-pagination',
   templateUrl: './pagination.component.html',
-  styleUrls: ['./pagination.component.scss']
+  styleUrls: ['./pagination.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class PaginationComponent implements OnChanges {
-  @Input() totalPages: number;
+export class PaginationComponent implements OnInit, OnDestroy {
+  public totalPages$ = new Subject<number>();
+  private destroy$ = new Subject();
+
+  @Input() set totalPages(number) {
+    console.log("input given");
+    this.totalPages$.next(number);
+  }
   @Output() goTo: EventEmitter<number> = new EventEmitter<number>();
 
   public currentPage = 1;
@@ -16,11 +23,23 @@ export class PaginationComponent implements OnChanges {
 
   constructor() { }
 
-  ngOnChanges(): void {
-    this.pages = [];
-    for (let i = 1; i <= this.totalPages; i++) {
-      this.pages.push(i);
-    }
+  ngOnInit(): void {
+    this.totalPages$.pipe(
+      takeUntil(this.destroy$)
+    )
+    .subscribe({
+      next: (noOfPages) => {
+        this.pages = [];
+        for (let i = 1; i <= noOfPages; i++) {
+          this.pages.push(i);
+        }
+      }
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
   public onGoTo(page: number): void {
